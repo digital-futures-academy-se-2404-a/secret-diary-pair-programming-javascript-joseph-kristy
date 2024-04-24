ffda9187054c926bbeb4ea5e5cf4d2d5ca51c40c
Joseph Mitchell <joejmitchell@hotmail.co.uk>
2024-04-24 11:59:21 +0100

    Finish user stories

--- spec/Diary.spec.js
+++ spec/Diary.spec.js
@@ -25,6 +25,75 @@ it("should not create entries when the diary is locked", () => {
     testDiary.createEntry(testEntry);
 
     // Assert
+    testDiary.unlockDiary("0123");
     expect(testDiary.getEntries()[0]).toBe(undefined);
 
+})
+
+it("should let user view entries when the diary is unlocked", () => {
+
+    // Arrange
+    const testDiary = new Diary();
+
+    // Act
+    const actual = testDiary.getEntries();
+
+    // Assert
+    expect(actual).not.toBe(undefined);
+
+})
+
+it("should not let user view entries when the diary is locked", () => {
+
+    // Arrange
+    const testDiary = new Diary();
+
+    // Act
+    testDiary.lockDiary();
+    const actual = testDiary.getEntries();
+
+    // Assert
+    expect(actual).toBe(undefined);
+
+})
+
+it("should allow user to lock the diary", () => {
+
+    // Arrange
+    const testDiary = new Diary();
+
+    // Act
+    testDiary.lockDiary();
+    const actual = testDiary.getLocked();
+
+    // Assert
+    expect(actual).toBeTrue();
+})
+
+it("should stay locked when the incorrect pin is passed", () => {
+
+    // Arrange
+    const testDiary = new Diary();
+
+    // Act
+    testDiary.lockDiary();
+    testDiary.unlockDiary("7895");
+    const actual = testDiary.getLocked();
+
+    // Assert
+    expect(actual).toBeTrue();
+})
+
+it("should unlock when the correct pin is passed", () => {
+
+    // Arrange
+    const testDiary = new Diary();
+
+    // Act
+    testDiary.lockDiary();
+    testDiary.unlockDiary("0123");
+    const actual = testDiary.getLocked();
+
+    // Assert
+    expect(actual).toBeFalse();
 })
--- src/Diary.js
+++ src/Diary.js
@@ -1,8 +1,24 @@
 
 export default class Diary { 
     #entries = [];
+    #isLocked;
+    #defaultPin = "0123";
     
-    createEntry = (entry) => this.#entries.push(entry);
+    createEntry = (entry) => {
+        !this.#isLocked && this.#entries.push(entry);
+    };
     
-    getEntries = () => this.#entries;
+    getEntries = () => {
+        if (!this.#isLocked) {
+            return this.#entries;
+        }      
+    };
+    
+    lockDiary = () => this.#isLocked = true;
+
+    getLocked = () => this.#isLocked;
+
+    unlockDiary = (pin) => {
+        if (pin === this.#defaultPin) this.#isLocked = false;
+    }
 }
